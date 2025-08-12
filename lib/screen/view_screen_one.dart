9aef00084c3880d479392b297ee27bdd4d4d73f4
Shaharyar <shaharyar4t4@gmail.com>
2025-08-13 00:50:50 +0500

    upadated

--- lib/screen/view_screen_one.dart
+++ lib/screen/view_screen_one.dart
@@ -5,6 +5,13 @@ class ViewScreenOne extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [Text("Heelo")],
+      ),
+    );
   }
 }
