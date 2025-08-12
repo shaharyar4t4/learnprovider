7bc0da598ece681d3304dab6f6017baf552f9036
Shaharyar <shaharyar4t4@gmail.com>
2025-08-13 00:47:02 +0500

    learning about provider

---
+++ android/app/src/main/kotlin/com/example/leranprovider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.leranprovider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/screen/view_screen_one.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewScreenOne extends StatelessWidget {
+  const ViewScreenOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
