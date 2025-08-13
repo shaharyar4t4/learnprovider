f90524505be089d4f03325a852e166cc1dd488e0
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-13 13:46:41 +0500

    updated

---
+++ lib/screen/count_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CountScreen extends StatefulWidget {
+  const CountScreen({super.key});
+
+  @override
+  State<CountScreen> createState() => _CountScreenState();
+}
+
+class _CountScreenState extends State<CountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
