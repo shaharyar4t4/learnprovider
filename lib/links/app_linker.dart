60b15f5b27a8640680714a313700aaef909a330b
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-22 11:40:57 +0500

    updated the presvious work

--- lib/links/app_linker.dart
+++ lib/links/app_linker.dart
@@ -1,7 +1,7 @@
 export 'package:flutter/material.dart';
 export 'package:provider/provider.dart';
-export 'package:leranprovider/before/view_screen_one.dart';
-export 'package:leranprovider/before/view_screen_two.dart';
+export 'package:leranprovider/basicstStateless&Stateful/view_screen_one.dart';
+export 'package:leranprovider/basicstStateless&Stateful/view_screen_two.dart';
 export 'package:leranprovider/screen/count_screen_example.dart';
 export 'package:flutter/foundation.dart';
 export 'package:leranprovider/provider/count_provider.dart';
