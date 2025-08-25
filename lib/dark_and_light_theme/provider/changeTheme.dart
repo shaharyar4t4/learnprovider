f41cd85915f5beb73bc287417b075398f89a7022
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-25 11:35:46 +0500

    learn about how to move screen in white theme into dark theme

---
+++ lib/dark_and_light_theme/provider/changeTheme.dart
@@ -0,0 +1,5 @@
+import 'package:leranprovider/links/app_linker.dart';
+
+class Changetheme extends ChangeNotifier{
+  
+}
---
+++ lib/dark_and_light_theme/screen/darkTheme.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:leranprovider/links/app_linker.dart';
+
+class Darktheme extends StatelessWidget {
+  const Darktheme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
