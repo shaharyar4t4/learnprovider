6044e8354544b335f29fce4e36cee11d27663452
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-25 14:14:45 +0500

    updated

---
+++ lib/statelessandstateful/screen/value_notify_listiner.dart
@@ -0,0 +1,10 @@
+import 'package:leranprovider/links/app_linker.dart';
+
+class ValueNotifyListiner extends StatelessWidget {
+  const ValueNotifyListiner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
