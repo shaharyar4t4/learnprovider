472420fb8a76edcf9a80cad91ac681f673420242
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-25 18:52:49 +0500

    updated

---
+++ lib/loginwithrestapi/provider/auth_provider.dart
@@ -0,0 +1,3 @@
+import 'package:leranprovider/links/app_linker.dart';
+
+class AuthProvider extends ChangeNotifier{}
---
+++ lib/loginwithrestapi/screen/loginScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Loginscreen extends StatefulWidget {
+  const Loginscreen({super.key});
+
+  @override
+  State<Loginscreen> createState() => _LoginscreenState();
+}
+
+class _LoginscreenState extends State<Loginscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+    );
+  }
+}
