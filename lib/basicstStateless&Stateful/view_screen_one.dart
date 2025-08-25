d2372456ca8e9c74cc6d2b8aa90131f7fb12883b
Shaharyar Ali <shaharyar4t4@gmail.com>
2025-08-25 13:22:56 +0500

    updated

--- lib/basicstStateless&Stateful/view_screen_one.dart
+++ lib/basicstStateless&Stateful/view_screen_one.dart
@@ -1,11 +1,17 @@
 
 import '../links/app_linker.dart';
 
-class ViewScreenOne extends StatelessWidget {
+class ViewScreenOne extends StatefulWidget {
 
    ViewScreenOne({super.key});
-   
+
+  @override
+  State<ViewScreenOne> createState() => _ViewScreenOneState();
+}
+
+class _ViewScreenOneState extends State<ViewScreenOne> {
   int num =0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
